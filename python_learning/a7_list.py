102f018179dbeecf8d207ab2b523d363673f6389
freshmea <freshmea@naver.com>
2024-01-15 15:55:39 +0900

    list2

--- python_learning/a7_list.py
+++ python_learning/a7_list.py
@@ -13,6 +13,12 @@ def main():
     list_d = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
     print(f"list_d[1][1] is {list_d[1][1]}.")
+    for i in range(10):
+        list_a.append(i * 2)
+    list_a.pop(5)
+    list_a.insert(5, 10.5)
+    print(list_a)
+    print(len(list_a))
 
 
 if __name__ == "__main__":
