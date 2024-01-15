b76ca269234685ae5cfd6152f59ad74d1f4a8451
freshmea <freshmea@naver.com>
2024-01-15 15:13:33 +0900

    type casting

---
+++ python_learning/a4_indexing.py
@@ -0,0 +1,28 @@
+def main():
+    s1 = "hello, world"
+    # print(s1[0])
+    # print(s1[1])
+    # print(s1[2])
+    # print(s1[3])
+    # print(s1[4])
+    # print(s1[5])
+    # print(s1[6])
+    # print(s1[7])
+    # print(s1[8])
+    # print(s1[9])
+    # print(s1[10])
+    # print(s1[11])
+    # print(s1[12])
+    # C style
+    # for i in range(len(s1)):
+    #     print(s1[i])
+    # Python style
+    # for s in s1:
+    #     print(s)
+
+    print(s1[0:5])
+    print(s1[7:12])
+
+
+if __name__ == "__main__":
+    main()
---
+++ python_learning/a5_input_type_casting.py
@@ -0,0 +1,12 @@
+def main():
+    input_var = str()
+    while not input_var.isdigit():
+        input_var = input("input a number: ")
+
+    print(type(input_var), input_var)
+    # print(input_var * 5)
+    print(int(input_var) * 5)
+
+
+if __name__ == "__main__":
+    main()
