c29ec77e730ff19ef18c5a13e575d062e379b4ec
freshmea <freshmea@naver.com>
2024-01-17 14:51:07 +0900

    imwrite

--- opencv_learning/a7_imshow.py
+++ opencv_learning/a7_imshow.py
@@ -2,7 +2,10 @@ import cv2
 
 
 def main():
-    img = cv2.imread("lena.jpg", 1)
+    img = cv2.imread(
+        "/home/aa/catholic_universe_opencv/opencv_learning/data/lena.jpg", 1
+    )
+    # img = cv2.imread("opencv_learning/a7_imshow.py", 1)
     cv2.imshow("lena", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
